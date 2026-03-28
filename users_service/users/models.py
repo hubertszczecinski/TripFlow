e90d08142eefe61441c2fa475c5bf8e74034b6bb
Martyna Łaganowska <martynaa.laganowska@gmail.com>
2026-03-28 19:18:28 +0100

    Implementation of RBAC

--- users_service/users/models.py
+++ users_service/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        EMPLOYEE = "EMPLOYEE"
+        FINANCE = "FINANCE"
+        HR = "HR"
+        ADMIN = "ADMIN"
+    role = models.CharField(max_length=30, default=Role.EMPLOYEE)
